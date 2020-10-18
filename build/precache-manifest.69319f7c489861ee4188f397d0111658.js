b26c2a941d4f232b1cc53ab2b2e17d54d7343ba9
赵嘉怡 <44658478+Ripenautumn@users.noreply.github.com>
2020-10-18 20:52:41 +0800

    Add files via upload

---
+++ build/precache-manifest.69319f7c489861ee4188f397d0111658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af469908ed9b9b4b6de728661865cd68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "057b27b20f0173716ca9",
+    "url": "/static/css/main.a3a8b971.chunk.css"
+  },
+  {
+    "revision": "9c169282175c28bb5d08",
+    "url": "/static/js/2.04aaf20c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04aaf20c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057b27b20f0173716ca9",
+    "url": "/static/js/main.b61ff38c.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
